117f7d93ff26fc69776ba30e555c5818a54f30b7
root <root@GVANNY.GVANNY>
2015-06-29 15:50:35 +0100

    Primera Página y favicon

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var path = require('path');
+
+//Se importan paquetes con middlewares
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Importamos enrrutadores
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
+//Creamos la aplicación
 var app = express();
 
 // view engine setup
@@ -15,7 +19,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+//Activamos el Favicon
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
